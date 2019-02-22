a16846db7d78b1bc65733b7330c5f6b75866431d
Piotr Pazoła <piotr@webtrip.pl>
2019-02-22 13:27:50 +0100

    isPiątunio? ficzer

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-/* test v. 0.11 */
+/* testGit Code Leszno v. 0.12 */
+let app = document.getElementById("app");
 window.onload = () => {
   doSomethingOnWindow();
   doSomethingOnConsole();
@@ -6,13 +7,29 @@ window.onload = () => {
 const doSomethingOnWindow = () => {
   let div = document.createElement("div");
   div.innerHTML='Ładuje...';
-  let app = document.getElementById("app");
   app.append(div);
   setTimeout(() => {
     div.innerHTML='Zajebisty napis z pozdrowieniami dla Code Leszno';
-    console.info('Done.');
+    console.info('Done. Pykło z kosmicznego API pozdrowienia.');
   },3000);
+  if(_isPiatunio()) {
+    _sendStringOnConsoleAndWindow('Dziś piąteczek!');
+  }
 }
 const doSomethingOnConsole = () => {
   console.warn('Ostrzeżenie na konsolę, poczekaj 3 sekundy na zmianę w dokumencie ;-)');
+}
+const _sendStringOnConsoleAndWindow = (string) => {
+  if(typeof string=="string") {
+    let div = document.createElement("div");
+    div.innerHTML=string;
+    app.append(div);
+    console.log(string);
+  }
+}
+const _isPiatunio = () => {
+  switch(new Date().getDay()) {
+    case 5: return true; break;
+    default: return false; break;
+  }
 }
