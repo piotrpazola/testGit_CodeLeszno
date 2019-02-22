b0957d7cf846c0779f68e86b1aab54569e2c9270
Piotr Pazoła <piotr@webtrip.pl>
2019-02-22 12:54:44 +0100

    Pierwszy commit * js+html na dzień dobry * jakiś README

---
+++ main.js
@@ -0,0 +1,18 @@
+/* test v. 0.1 */
+window.onload = () => {
+  doSomethingOnWindow();
+  doSomethingOnConsole();
+}
+const doSomethingOnWindow = () => {
+  let div = document.createElement("div");
+  div.innerHTML='Ładuje...';
+  let app = document.getElementById("app");
+  app.append(div);
+  setTimeout(() => {
+    div.innerHTML='Zajebisty napis z pozdrowieniami dla Code Leszno';
+    console.info('Done. Pykło.');
+  },3000);
+}
+const doSomethingOnConsole = () => {
+  console.warn('Ostrzeżenie na konsolę, poczekaj 3 sekundy na zmianę w dokumencie ;-)');
+}
