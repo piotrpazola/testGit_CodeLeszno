34079a2dd014a96cbcaffdeb48e3e61a20b827dd
Piotr Pazoła <piotr@webtrip.pl>
2019-02-25 11:04:57 +0100

    using generator as async func with for await of syntax

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-/* testGit Code Leszno v. 0.12 */
+/* testGit Code Leszno v. 0.14 */
 let app = document.getElementById("app");
 window.onload = () => {
   doSomethingOnWindow();
   doSomethingOnConsole();
+  doSomethingThroughGenerator();
 }
 const doSomethingOnWindow = () => {
   let div = document.createElement("div");
@@ -19,6 +20,14 @@ const doSomethingOnWindow = () => {
 const doSomethingOnConsole = () => {
   console.warn('Ostrzeżenie na konsolę, poczekaj 3 sekundy na zmianę w dokumencie ;-)');
 }
+const doSomethingThroughGenerator = async () => {
+  const iterator = _someGeneratorFunctionExample();
+  for await(let value of iterator) {
+    let div = document.createElement("div");
+    div.innerHTML=value;
+    app.append(div);
+  }
+}
 const _sendStringOnConsoleAndWindow = (string) => {
   if(typeof string=="string") {
     let div = document.createElement("div");
@@ -31,5 +40,11 @@ const _isPiatunio = () => {
   switch(new Date().getDay()) {
     case 5: return true; break;
     default: return false; break;
+  }
+}
+const _someGeneratorFunctionExample = async function* () {
+  for(let i=0; i<10; i++) {
+    await new Promise(resolve => setTimeout(resolve, 500));
+    yield 'To jest wiersz nr '+(i+1)+' z generatora';
   }
 }
